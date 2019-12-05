8e8eb4e67cd6dc2ecee13322b68bf0fd6b592b7d
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-12-04 21:37:10 -0300

    Make Declaration an interface

--- src/main/java/calculator/domain/Declaration.java
+++ src/main/java/calculator/domain/Declaration.java
@@ -1,17 +1,5 @@
 package calculator.domain;
 
-public class Declaration {
-    private double deduction;
-
-    public Declaration(double deduction) {
-        setDeduction(deduction);
-    }
-
-    private void setDeduction(double deduction) {
-        this.deduction = deduction;
-    }
-
-    public double deduction() {
-        return (double) this.deduction;
-    }
+public interface Declaration {
+    
 }
